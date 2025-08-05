b171d9846bcea41ad4848647203fc9b74ec129c2
K LOKESH <lokesh.kandavel04.@gmail.com>
2025-08-05 21:17:13 +0530

    time update

--- backend/models/Bus.js
+++ backend/models/Bus.js
@@ -4,6 +4,7 @@ const stopSchema = new mongoose.Schema({
   name: { type: String, required: true },
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true },
+  time : { type: String, required: true },
 });
 
 const busSchema = new mongoose.Schema({
